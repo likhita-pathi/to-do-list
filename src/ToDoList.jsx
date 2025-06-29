e0ad5179863e59df478ba546052efada4446e3aa
Likhita Pathi <likhitapathi0@gmail.com>
2025-06-29 17:15:26 +0530

    Writing basic structure of Todo component

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+function ToDoList() {
+  const [tasks, setTasks] = useState(["Eat Breakfast", "Take a Shower"]);
+  const [newTask, setNewTask] = useState("");
 
-export default function ToDoList() {
-  return <div>Todo</div>;
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onchange={handleInputChange}
+        />
+        <button classname="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              👆
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              👇
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
+export default ToDoList;
