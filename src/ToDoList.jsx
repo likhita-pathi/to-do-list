81dd7115ac01a3ae4e213cc8d6b9f305a9fc2cc7
Likhita Pathi <likhitapathi0@gmail.com>
2025-06-29 17:16:48 +0530

    Adding changing task order feature

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -16,6 +16,26 @@ function ToDoList() {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
   }
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
   return (
     <div className="to-do-list">
       <h1>To-Do-List</h1>
